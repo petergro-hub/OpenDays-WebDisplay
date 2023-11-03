9a578ce9acd071589b1a00f5cfede5bd4964e61e
3090 <3090@ivrl>
2023-11-03 16:25:32 +0100

    edited webbrowser, working

--- app.py
+++ app.py
@@ -1,42 +1,30 @@
+from flask import Flask, send_from_directory, jsonify
 import os
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-from flask import Flask, render_template, send_from_directory
+from threading import Timer
+import time
 
 app = Flask(__name__)
+image_folder = '/home/ivrl/Fooocus/outputs'
+image_list = []
 
-# Change this to the path of your custom image folder
-IMAGE_FOLDER = '/home/peter/OpenDays/otherApp/static/images'
-
-class ImageHandler(FileSystemEventHandler):
-    def __init__(self, app):
-        self.app = app
-    def on_any_event(self, event):
-        if event.is_directory:
-            return None
-        elif event.event_type == 'created':
-            self.update_image_list()
-        elif event.event_type == 'deleted':
-            self.update_image_list()
-        elif event.event_type == 'modified':
-            self.update_image_list()
-    def update_image_list(self):
-        images = sorted(os.listdir(IMAGE_FOLDER))
-        self.app.jinja_env.globals['images'] = images
+def update_image_list():
+    global image_list
+    # List all image files in the folder
+    image_list = [f for f in os.listdir(image_folder) if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    Timer(5.0, update_image_list).start()
 
 @app.route('/')
 def index():
-    images = sorted(os.listdir(IMAGE_FOLDER))
-    return render_template('index.html', images=images)
+    return send_from_directory('templates', 'index.html')
+
+@app.route('/images')
+def images():
+    return jsonify(images=image_list)
 
 @app.route('/image/<filename>')
-def get_image(filename):
-    return send_from_directory(IMAGE_FOLDER, filename)
+def image(filename):
+    return send_from_directory(image_folder, filename)
 
 if __name__ == '__main__':
-    event_handler = ImageHandler(app)
-    observer = Observer()
-    observer.schedule(event_handler, path=IMAGE_FOLDER)
-    observer.start()
-    app.jinja_env.globals['images'] = []
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    update_image_list()  # Start the image list update loop
+    app.run(host='0.0.0.0', port=8000)
